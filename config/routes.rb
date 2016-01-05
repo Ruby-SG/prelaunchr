a543c51ec46e8c0fd1a926ac503c567022dedb99
JackyVo <jackyinvn@gmail.com>
2016-01-05 18:23:26 +0900

    Add invite page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Prelaunchr::Application.routes.draw do
 
   match 'privacy-policy' => 'users#policy'
 
+  match 'invite-friends' => 'users#invite', as: :invite_friends
+
   unless Rails.application.config.consider_all_requests_local
       match '*not_found', to: 'users#redirect', :format => false
   end
