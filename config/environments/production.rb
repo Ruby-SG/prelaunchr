0e250989d19f435bcce216dac6c88868f91d76af
JackyVo <jackyinvn@gmail.com>
2016-01-05 18:54:32 +0900

    Setup email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,13 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'www.example.com' }
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'prelaunchc-app.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+    address:   'smtp.mandrillapp.com',
+    port:      587,
+    user_name: 'dev@ideabox.com.au',
+    password:  'HY0FhQQQtPfZxZt8ile5PA'
+  }
 end
