b0bfb2a9389b071681a23dbbf8d011bbd13c61e7
JackyVo <jackyinvn@gmail.com>
2016-01-05 17:47:53 +0900

    Update facebook image and fav icon

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,32 +10,32 @@ class User < ActiveRecord::Base
     before_create :create_referral_code
     after_create :send_welcome_email
 
-    REFERRAL_STEPS = [
-        {
-            'count' => 5,
-            "html" => "1 Prayer Hammer eBook",
-            "class" => "two",
-            "image" =>  ActionController::Base.helpers.asset_path("refer/5.png")
-        },
-        {
-            'count' => 10,
-            "html" => "1 Prayer Hammer eBook<br/>+ 1 Prayer Eagle Badge (Red)",
-            "class" => "three",
-            "image" => ActionController::Base.helpers.asset_path("refer/10.png")
-        },
-        {
-            'count' => 25,
-            "html" => "1 Prayer Hammer eBook<br/>+ 2 Prayer Eagle Badges<br/>(Red & White)",
-            "class" => "four",
-            "image" => ActionController::Base.helpers.asset_path("refer/25.png")
-        },
-        {
-            'count' => 50,
-            "html" => "1 Prayer Hammer eBook<br/>+ 2 Prayer Eagle Badges<br/>(Red & White)<br/><b>+ 1 Year FREE Membership</b>",
-            "class" => "five",
-            "image" => ActionController::Base.helpers.asset_path("refer/50.png")
-        }
-    ]
+    # REFERRAL_STEPS = [
+    #     {
+    #         'count' => 5,
+    #         "html" => "1 Prayer Hammer eBook",
+    #         "class" => "two",
+    #         "image" =>  ActionController::Base.helpers.asset_path("refer/5.png")
+    #     },
+    #     {
+    #         'count' => 10,
+    #         "html" => "1 Prayer Hammer eBook<br/>+ 1 Prayer Eagle Badge (Red)",
+    #         "class" => "three",
+    #         "image" => ActionController::Base.helpers.asset_path("refer/10.png")
+    #     },
+    #     {
+    #         'count' => 25,
+    #         "html" => "1 Prayer Hammer eBook<br/>+ 2 Prayer Eagle Badges<br/>(Red & White)",
+    #         "class" => "four",
+    #         "image" => ActionController::Base.helpers.asset_path("refer/25.png")
+    #     },
+    #     {
+    #         'count' => 50,
+    #         "html" => "1 Prayer Hammer eBook<br/>+ 2 Prayer Eagle Badges<br/>(Red & White)<br/><b>+ 1 Year FREE Membership</b>",
+    #         "class" => "five",
+    #         "image" => ActionController::Base.helpers.asset_path("refer/50.png")
+    #     }
+    # ]
 
     private
 
