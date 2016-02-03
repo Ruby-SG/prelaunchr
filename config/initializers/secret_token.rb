c3603d926ff7cc557503b230a70212fc8f6a26ab
JackyVo <jackyinvn@gmail.com>
2016-02-03 10:50:19 +0700

    Pre

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Prelaunchr::Application.config.secret_key = 'bbd60160589e16816525e3406a0669da1cf82749dd1b4359a5b7015835f16beb86ae8c8c4ba4aea971d96c956b5a0c4aa6a4f9118c1b338f469bfe60573a5d74'
+Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || 'xxxxxxxx-change-me-please-xxxxxxxx'
