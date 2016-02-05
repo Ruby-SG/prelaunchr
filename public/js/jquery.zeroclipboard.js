80f01fb498555251384c73e3b7d9dfc937f8fe73
JackyVo <jackyinvn@gmail.com>
2016-02-05 18:07:41 +0700

    Update zero clipboard

--- public/js/jquery.zeroclipboard.js
+++ public/js/jquery.zeroclipboard.js
@@ -6,9 +6,6 @@ client.on( "ready", function( readyEvent ) {
   // alert( "ZeroClipboard SWF is ready!" );
 
   client.on( "aftercopy", function( event ) {
-    // `this` === `client`
-    // `event.target` === the element that was clicked
-    event.target.style.display = "none";
-    alert("Copied text to clipboard: " + event.data["text/plain"] );
+    alert("Copied to clipboard.");
   } );
 } );
