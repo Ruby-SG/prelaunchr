183cf4ffd9579be807ddfde1f9fa665b2708702c
JackyVo <jackyinvn@gmail.com>
2016-02-05 13:56:18 +0700

    Add copy

---
+++ public/js/jquery.zeroclipboard.js
@@ -0,0 +1,14 @@
+var client = new ZeroClipboard( $("#click-to-copy"), {
+              moviePath: "ZeroClipboard.swf",
+              debug: false
+} );
+client.on( "ready", function( readyEvent ) {
+  // alert( "ZeroClipboard SWF is ready!" );
+
+  client.on( "aftercopy", function( event ) {
+    // `this` === `client`
+    // `event.target` === the element that was clicked
+    event.target.style.display = "none";
+    alert("Copied text to clipboard: " + event.data["text/plain"] );
+  } );
+} );
