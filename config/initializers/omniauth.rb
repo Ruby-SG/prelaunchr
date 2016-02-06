dadda4cfc2fe49f89c6a48a477e4b3d355dc7889
JackyVo <jackyinvn@gmail.com>
2016-02-06 07:59:56 +0700

    Change config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1751367148426041', '5a303ee98bce3b06c2af6305b50116fd', {:provider_ignores_state => true}
+  provider :facebook, '696018187184969', '71069401edc1a34a80ce9f7c79240c53', {:provider_ignores_state => true}
 end
