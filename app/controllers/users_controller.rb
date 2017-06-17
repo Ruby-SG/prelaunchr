e119d35e372e7d8c684d94c7f994a845c86fcbc3
JackyVo <jackyinvn@gmail.com>
2017-06-17 20:45:16 +0700

    Update layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,140 +3,107 @@ require 'net/https'
 require 'uri'
 
 class UsersController < ApplicationController
-    before_filter :skip_first_page, :only => :new
+	before_filter :skip_first_page, :only => :new
 
-    def new
-        @bodyId = 'home'
-        @is_mobile = mobile_device?
+	def new
+		@bodyId = 'home'
+		@is_mobile = mobile_device?
 
-        @user = User.new
+		@user = User.new
 
-        respond_to do |format|
-            format.html # new.html.erb
-        end
-    end
+		respond_to do |format|
+			format.html # new.html.erb
+		end
+	end
 
-    def create
-        url = nil
-        # Get user to see if they have already signed up
-        @user = User.find_by_email(params[:user][:email])
+	def create
+		url = nil
+		# Get user to see if they have already signed up
+		@user = User.find_by_email(params[:user][:email])
 
-        # If user doesnt exist, make them, and attach referrer
-        if @user.nil?
+		# If user doesnt exist, make them, and attach referrer
+		if @user.nil?
 
-            # cur_ip = IpAddress.find_by_address(request.env['HTTP_X_FORWARDED_FOR'])
+			# cur_ip = IpAddress.find_by_address(request.env['HTTP_X_FORWARDED_FOR'])
 
-            # if !cur_ip
-            #     cur_ip = IpAddress.create(
-            #         :address => request.env['HTTP_X_FORWARDED_FOR'],
-            #         :count => 0
-            #     )
-            # end
+			# if !cur_ip
+			#     cur_ip = IpAddress.create(
+			#         :address => request.env['HTTP_X_FORWARDED_FOR'],
+			#         :count => 0
+			#     )
+			# end
 
-            # if cur_ip.count > 2
-            #     return redirect_to root_path
-            # else
-            #     cur_ip.count = cur_ip.count + 1
-            #     cur_ip.save
-            # end
+			# if cur_ip.count > 2
+			#     return redirect_to root_path
+			# else
+			#     cur_ip.count = cur_ip.count + 1
+			#     cur_ip.save
+			# end
 
-            @user = User.new(:email => params[:user][:email])
+			@user = User.new(:email => params[:user][:email])
 
-            @referred_by = User.find_by_referral_code(cookies[:h_ref])
+			@referred_by = User.find_by_referral_code(cookies[:h_ref])
 
-            puts '------------'
-            puts @referred_by.email if @referred_by
-            puts params[:user][:email].inspect
-            puts request.env['HTTP_X_FORWARDED_FOR'].inspect
-            puts '------------'
+			puts '------------'
+			puts @referred_by.email if @referred_by
+			puts params[:user][:email].inspect
+			puts request.env['HTTP_X_FORWARDED_FOR'].inspect
+			puts '------------'
 
-            if !@referred_by.nil?
-                @user.referrer = @referred_by
-            end
+			if !@referred_by.nil?
+				@user.referrer = @referred_by
+			end
 
-            @user.save
+			@user.save
+		end
 
-            puts 'send emails ######'
+		# Send them over refer action
+		respond_to do |format|
+			if !@user.nil?
+				cookies[:h_email] = { :value => @user.email }
+				format.html { redirect_to '/refer-a-friend' }
+			else
+				format.html { redirect_to root_path, :alert => "Something went wrong!" }
+			end
+		end
+	end
 
-            # post to emails list ##
-            url = post_emails(@user.email)
-        end
+	def refer
+		email = cookies[:h_email]
 
-        # Send them over refer action
-        respond_to do |format|
+		@bodyId = 'refer'
+		@is_mobile = mobile_device?
 
-            if !@user.nil?
-                cookies[:h_email] = { :value => @user.email }
-                redirect_to url and return if url
-                format.html { redirect_to '/refer-a-friend' }
-            else
-                format.html { redirect_to root_path, :alert => "Something went wrong!" }
-            end
-        end
-    end
+		@user = User.find_by_email(email)
 
-    def refer
-        email = cookies[:h_email]
+		respond_to do |format|
+			if !@user.nil?
+				format.html #refer.html.erb
+			else
+				format.html { redirect_to root_path, :alert => "Something went wrong!" }
+			end
+		end
+	end
 
-        @bodyId = 'refer'
-        @is_mobile = mobile_device?
+	def policy
 
-        @user = User.find_by_email(email)
+	end
 
-        respond_to do |format|
-            if !@user.nil?
-                format.html #refer.html.erb
-            else
-                format.html { redirect_to root_path, :alert => "Something went wrong!" }
-            end
-        end
-    end
+	def redirect
+		redirect_to root_path, :status => 404
+	end
 
-    def policy
+	private
 
-    end
-
-    def redirect
-        redirect_to root_path, :status => 404
-    end
-
-    private
-
-    def skip_first_page
-        if !Rails.application.config.ended
-            email = cookies[:h_email]
-            if email and !User.find_by_email(email).nil?
-                redirect_to '/refer-a-friend'
-            else
-                cookies.delete :h_email
-            end
-        end
-    end
-
-    def post_emails email
-      puts 'POSTING TO EMAIL LIST ######'
-
-      params = {
-        meta_web_form_id:'256425572',
-        listname:'optionsbox',
-        redirect: 'http://rubysg-demo.herokuapp.com/refer-a-friend',
-        meta_redirect_onlist: 'http://rubysg-demo.herokuapp.com/refer-a-friend',
-        meta_adtracking:'OptionsBox-2015',
-        meta_message:'1',
-        meta_required:'email',
-        meta_tooltip:'email||Enter Email Here...',
-        email: email
-      }
-
-      x = Net::HTTP.post_form(URI.parse('https://www.aweber.com/scripts/addlead.pl'), params)
-      puts 'Getting here ###############'
-      str = x.body
-      p str
-
-      url = str[/https:\/\/www.aweber.com\/form-captcha.+>here/]
-      url = url.gsub('">here', '').gsub('&amp;', '&') if url
-
-      return url
-    end
+	def skip_first_page
+		if !Rails.application.config.ended
+			email = cookies[:h_email]
+			if email and !User.find_by_email(email).nil?
+				redirect_to '/refer-a-friend'
+			else
+				cookies.delete :h_email
+			end
+		end
+	end
 
 end
